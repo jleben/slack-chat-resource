741c5b6907e4458257f399025ebaf935e1f30673
Jakob Leben <jakob.leben@gmail.com>
2018-04-02 15:33:15 -0700

    post/check: Echo request version in response

--- post/check/main.go
+++ post/check/main.go
@@ -2,9 +2,35 @@ package main
 
 import (
     "os"
+    "fmt"
+    "encoding/json"
 )
 
 func main() {
-    os.Stderr.WriteString("Not supported.\n")
+    var request map[string]interface{}
+
+    {
+        err := json.NewDecoder(os.Stdin).Decode(&request)
+        if err != nil {
+            fatal("parsing request", err)
+        }
+    }
+
+    response := []interface{}{}
+
+    if request["version"] != nil {
+        response = append(response, request["version"])
+    }
+
+    {
+        err := json.NewEncoder(os.Stdout).Encode(&response)
+        if err != nil {
+            fatal("serializing response", err)
+        }
+    }
+}
+
+func fatal(doing string, err error) {
+    fmt.Fprintf(os.Stderr, "Error " + doing + ": " + err.Error() + "\n")
     os.Exit(1)
 }
